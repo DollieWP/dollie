51e48d362281b82e4de690f2eebee620b6edb14f
Gabriel Fratica <gabriel@seventhqueen.com>
2020-02-21 12:10:55 +0200

    Automatically generate .pot file for translations

---
+++ gulpfile.babel.js
@@ -0,0 +1,10 @@
+/* eslint-env es6 */
+'use strict';
+
+// External dependencies
+import {parallel, series} from 'gulp';
+
+// Internal dependencies
+import translate from './gulp/translate';
+
+export default translate;
