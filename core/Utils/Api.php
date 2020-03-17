a28f75ce13785b64ef45028b9251216637f2de32
George Tudor <georgebitq@gmail.com>
2020-03-17 11:58:47 +0200

    define routes

--- core/Utils/Api.php
+++ core/Utils/Api.php
@@ -15,11 +15,46 @@ use Dollie\Core\Singleton;
  */
 class Api extends Singleton {
 
+	const
+		ROUTE_CONTAINER_GET = 'containers',
+		ROUTE_CONTAINER_CREATE = 'containers/create',
+		ROUTE_CONTAINER_TRIGGER = 'containers/trigger',
+
+		ROUTE_DOMAIN_ROUTES_ADD = 'domain/routes/add',
+		ROUTE_DOMAIN_ROUTES_GET = 'domain/routes/get',
+		ROUTE_DOMAIN_ROUTES_DELETE = 'domain/routes/delete',
+		ROUTE_DOMAIN_INSTALL_LETSENCRYPT = 'domain/install/letsencrypt',
+		ROUTE_DOMAIN_INSTALL_CLOUDFLARE = 'domain/instlal/cloudflare',
+
+		ROUTE_BACKUP_GET = 'backup',
+		ROUTE_BACKUP_CREATE = 'backup/create',
+		ROUTE_BACKUP_RESTORE = 'backup/restore',
+
+		ROUTE_BLUEPRINT_GEt = 'blueprint',
+		ROUTE_BLUEPRINT_CREATE_OR_UPDATE = 'blueprint/create-or-update',
+		ROUTE_BLUEPRINT_DEPLOY = 'blueprint/deploy',
+		ROUTE_BLUEPRINT_DEPLOY_FOR_PARTNER = 'blueprint/deploy-partner',
+
+		ROUTE_PLUGINS_INSTALL = 'plugins/install',
+		ROUTE_PLUGINS_UPDATES_GET = 'plugins/updates/get',
+		ROUTE_PLUGINS_UPDATES_APPLY = 'plugins/updates/apply',
+
+		ROUTE_NODES_GET = 'nodes',
+		ROUTE_WIZARD_SETUP = 'setup';
+
 	/**
 	 * Api constructor.
 	 */
 	public function __construct() {
 		parent::__construct();
+	}
+
+	public function get( $endpoint ) {
+		return wp_remote_get( $endpoint );
+	}
+
+	public function post( $endpoint, $data = [] ) {
+
 	}
 
 	/**
