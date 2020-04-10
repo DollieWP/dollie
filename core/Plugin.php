492aebd1e1872e99f36a9932b2ccd59c05df670d
Gabriel Fratica <gabriel@seventhqueen.com>
2020-04-10 13:17:25 +0300

    change offlline to offline_access

--- core/Plugin.php
+++ core/Plugin.php
@@ -225,7 +225,7 @@ class Plugin extends Singleton {
                 <div class="dollie-msg-button-right">
 					<?php
 					printf( '<a href="%s">%s</a>',
-						'https://oauth2.staging.str5.net/hydra-public/oauth2/auth?client_id=dollie-wp-plugin&redirect_uri=https%3A%2F%2Fpartners.getdollie.com%2Fcallback&response_type=code&scope=offline+api.read&state=' . substr( md5( uniqid( mt_rand(), true ) ), 0, 10 ) . '&redirect_origin=' . urlencode( admin_url( 'admin.php?page=wpd_api' ) ) . '&api_url=' . urlencode( admin_url( 'admin-ajax.php' ) ),
+						'https://oauth2.staging.str5.net/hydra-public/oauth2/auth?client_id=dollie-wp-plugin&redirect_uri=https%3A%2F%2Fpartners.getdollie.com%2Fcallback&response_type=code&scope=offline_access+api.read&state=' . substr( md5( uniqid( mt_rand(), true ) ), 0, 10 ) . '&redirect_origin=' . urlencode( admin_url( 'admin.php?page=wpd_api' ) ) . '&api_url=' . urlencode( admin_url( 'admin-ajax.php' ) ),
 						__( 'Click here', DOLLIE_DOMAIN ) );
 					?>
                 </div>
