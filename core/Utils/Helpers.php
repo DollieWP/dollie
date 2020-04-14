9a9f40307181dd5c00ff505c5eb25fffbd91c266
Gabriel Fratica <gabriel@seventhqueen.com>
2020-04-14 18:36:07 +0300

    fix the way we get the acf field groups for newer acf pro versions

--- core/Utils/Helpers.php
+++ core/Utils/Helpers.php
@@ -333,7 +333,7 @@ class Helpers extends Singleton {
 	public function acf_get_database_field_group_keys() {
 
 		$keys   = [];
-		$groups = apply_filters( 'acf/get_field_groups', [] );
+		$groups = acf_get_field_groups();
 
 		foreach ( $groups as $group ) {
 			$keys[ $group['title'] ] = $group['key'];
