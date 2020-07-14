41012e642b75498a17cb2a65f260aca65d5b6088
George Tudor <georgebitq@gmail.com>
2020-07-14 14:24:57 +0300

    add docs to delete site class

--- core/Modules/Forms/DeleteSite.php
+++ core/Modules/Forms/DeleteSite.php
@@ -10,7 +10,6 @@ use Dollie\Core\Log;
 use Dollie\Core\Modules\ContainerManagement;
 use Dollie\Core\Modules\Forms;
 use Dollie\Core\Singleton;
-use Dollie\Core\Utils\Api;
 
 /**
  * Class DeleteSite
@@ -29,8 +28,10 @@ class DeleteSite extends Singleton {
 		add_action( 'acf/init', [ $this, 'acf_init' ] );
 	}
 
+	/**
+	 * Init ACF
+	 */
 	public function acf_init() {
-
 		// Form args
 		add_filter( 'af/form/args/key=' . $this->form_key, [ $this, 'change_form_args' ] );
 
@@ -39,6 +40,13 @@ class DeleteSite extends Singleton {
 		add_action( 'af/form/before_submission/key=' . $this->form_key, [ $this, 'submission_callback' ], 10, 3 );
 	}
 
+	/**
+	 * Form callback
+	 *
+	 * @param $form
+	 * @param $fields
+	 * @param $args
+	 */
 	public function submission_callback( $form, $fields, $args ) {
 		$container = Forms::get_form_container();
 
@@ -49,16 +57,27 @@ class DeleteSite extends Singleton {
 		ContainerManagement::instance()->container_action( 'stop', $container->id );
 	}
 
+	/**
+	 * Form validation
+	 *
+	 * @param $form
+	 * @param $args
+	 */
 	public function validate_form( $form, $args ) {
-
 		$container = Forms::get_form_container();
 
-		if ( ! af_get_field( 'confirm_site_name' ) || af_get_field('confirm_site_name') !== $container->slug ) {
+		if ( ! af_get_field( 'confirm_site_name' ) || af_get_field( 'confirm_site_name' ) !== $container->slug ) {
 			af_add_error( 'confirm_site_name', esc_html__( 'Please type the unique name of your site. Your site name is shown in the sidebar and in your URL address bar.', 'dollie' ) );
 		}
-
 	}
 
+	/**
+	 * Change submit button text
+	 *
+	 * @param $args
+	 *
+	 * @return mixed
+	 */
 	public function change_form_args( $args ) {
 		$args['submit_text'] = esc_html__( 'Delete', 'dollie' );
 
