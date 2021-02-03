dc5ca364d12560c4ced893497325ff36d0e90598
Gabriel Fratica <gabriel@seventhqueen.com>
2021-02-03 15:34:59 +0200

    site remote info url update

--- core/Elementor/Tags/SiteRemoteInfoUrl.php
+++ core/Elementor/Tags/SiteRemoteInfoUrl.php
@@ -2,115 +2,98 @@
 
 namespace Dollie\Core\Elementor\Tags;
 
+use Dollie\Core\Modules\Container;
 use Elementor\Controls_Manager;
 use Elementor\Core\DynamicTags\Tag;
 
 
-class SiteRemoteInfoUrl extends Tag
-{
+class SiteRemoteInfoUrl extends Tag {
 
 	private $wpd_data = [];
 
-	public function __construct(array $data = [])
-	{
-		parent::__construct($data);
+	public function __construct( array $data = [] ) {
+		parent::__construct( $data );
 
 		$current_id = dollie()->get_current_site_id();
 
 		//Get Items from Feed
-		$this->wpd_data = \Dollie\Core\Modules\Container::instance()->get_container_details($current_id);
+		$this->wpd_data                                  = Container::instance()->get_container_details( $current_id );
 		$this->wpd_data['customer_data']["Support Link"] = dollie()->get_support_link();
-		$this->wpd_data['site_data']["URL"] = dollie()->get_container_url($current_id);
+		$this->wpd_data['site_data']["URL"]              = dollie()->get_container_url( $current_id );
 
 	}
 
-	public function get_name()
-	{
+	public function get_name() {
 
 		return 'dollie-site-info-url';
 	}
 
-	public function get_title()
-	{
-		return __('Dollie Site Remote Info URL', 'dynamic-tags');
+	public function get_title() {
+		return __( 'Dollie Site Remote Info URL', 'dollie' );
 	}
 
 
-	public function get_group()
-	{
+	public function get_group() {
 		return 'dollie-tags';
 	}
 
-	public function get_categories()
-	{
-		return [\Elementor\Modules\DynamicTags\Module::URL_CATEGORY];
+	public function get_categories() {
+		return [ \Elementor\Modules\DynamicTags\Module::URL_CATEGORY ];
 	}
 
-	protected function _register_controls()
-	{
+	protected function _register_controls() {
 
 
 		$keys = [];
-		foreach ($this->wpd_data['site_data'] as $k => $data) {
+		foreach ( $this->wpd_data['site_data'] as $k => $data ) {
 
-			if (is_array($data) || $data === false) {
+			if ( is_array( $data ) || $data === false ) {
 				continue;
 			}
 
-			if (
-				! filter_var($data, FILTER_VALIDATE_URL)
-			) {
+			if ( ! filter_var( $data, FILTER_VALIDATE_URL ) ) {
 				continue;
 			}
 
-			$keys[$k] = 'Site - ' . $k;
+			$keys[ $k ] = 'Site - ' . $k;
 		}
 
-		foreach ($this->wpd_data['customer_data'] as $k => $data) {
+		foreach ( $this->wpd_data['customer_data'] as $k => $data ) {
 
-			if (is_array($data) || $data === false) {
+			if ( is_array( $data ) || $data === false ) {
 				continue;
 			}
 
-			$keys[$k] = $k;
+			$keys[ $k ] = $k;
 		}
-
-		//Our Basic WP Feed
-
-		// $extra_tags = array(
-		// 	"New Data" => 'value',
-		// );
-
-		//$keys = array_merge($keys, $extra_tags);
-
 
 		$this->add_control(
 			'param_name',
 			[
-				'label'   => __('Choose Data', 'elementor-pro'),
+				'label'   => __( 'Choose Data', 'dollie' ),
 				'type'    => Controls_Manager::SELECT,
 				'options' => $keys,
 			]
 		);
 	}
 
-	public function render()
-	{
 
-		$param_name = $this->get_settings('param_name');
+	public function render() {
 
-		if (!$param_name) {
-			return '';
+		$param_name = $this->get_settings( 'param_name' );
+
+		if ( ! $param_name ) {
+			return;
 		}
 
 		$data = $this->wpd_data['site_data'];
 
-		if (!isset($data[$param_name])) {
-			return '';
+		if ( ! isset( $data[ $param_name ] ) ) {
+			return;
 		}
 
-		$value = $data[$param_name];
+		$value = $data[ $param_name ];
 
-		echo wp_kses_post($value);
+		echo wp_kses_post( $value );
 	}
 }
