db3ec5d0c7c368ecd99d7e46ec4e9b9068c74585
Bowe Frankema <bowe@cfcommunity.net>
2021-02-10 17:17:13 +0100

    Enqueue alpine for sites nav
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- templates/widgets/general/sites-navigation.php
+++ templates/widgets/general/sites-navigation.php
@@ -1,5 +1,7 @@
 <?php
-
+if (!\Elementor\Plugin::instance()->editor->is_edit_mode()) {
+	wp_enqueue_script('dollie-layout-alpine');
+}
 use Dollie\Core\Modules\AccessControl;
 
 if (get_field('wpd_allow_site_dashboard_access', 'options') === 0 && !current_user_can('manage_options')) {
@@ -23,7 +25,7 @@ $containers = new WP_Query(
 	<ul class="dol-list-none dol-p-0 dol-m-0 dol-widget-sites-nav">
 		<li class="dol-m-0">
 			<span class="dol-block dol-font-bold dol-text-primary-400 dol-uppercase dol-py-2">
-				<?php _e('Sites', 'dollie'); ?>
+				<?php echo dollie()->get_sites_page_title(); ?>
 			</span>
 		</li>
 		<?php
@@ -49,7 +51,7 @@ $containers = new WP_Query(
 				'delete'          => '<i class="fas fa-trash-alt"></i>' . __('Delete', 'dollie'),
 			];
 
-			if ( dollie()->is_blueprint(get_the_ID()) ) {
+			if (dollie()->is_blueprint(get_the_ID())) {
 				unset($menu['domains']);
 			}
 
@@ -62,50 +64,50 @@ $containers = new WP_Query(
 				continue;
 			} ?>
 
-				<div x-data="{ open: false }">
-					<span @click="open = !open" class="dol-flex dol-w-full dol-justify-between dol-items-center dol-nav-btn dol-nav-btn-secondary dol-cursor-pointer">
-						<span class="dol-flex dol-items-center">
-							<span class="dol-font-medium">
-								<?php
-								if (!empty($domain)) {
-									echo '<i class="fas fa-globe"></i>';
-								} elseif (!empty($blueprint)) {
-									echo '<i class="fas fa-copy"></i>';
-								} else {
-									echo '<i class="fas fa-cog"></i>';
-								}
-								?>
-								<?php
-								if (!empty($domain)) {
-									echo esc_html($domain);
-								} else {
-									echo get_the_title();
-								}
-								?>
-							</span>
-						</span>
-						<span>
-							<svg class="dol-h-4 dol-w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-								<path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
-								<path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
-							</svg>
+			<div x-data="{ open: false }">
+				<span @click="open = !open" class="dol-flex dol-w-full dol-justify-between dol-items-center dol-nav-btn dol-nav-btn-secondary dol-cursor-pointer">
+					<span class="dol-flex dol-items-center">
+						<span class="dol-font-medium">
+							<?php
+							if (!empty($domain)) {
+								echo '<i class="fas fa-globe"></i>';
+							} elseif (!empty($blueprint)) {
+								echo '<i class="fas fa-copy"></i>';
+							} else {
+								echo '<i class="fas fa-cog"></i>';
+							}
+							?>
+							<?php
+							if (!empty($domain)) {
+								echo esc_html($domain);
+							} else {
+								echo get_the_title();
+							}
+							?>
 						</span>
 					</span>
-					<div x-show="open" class="dol-p-2" style="display: none;">
-						<?php foreach ($menu as $page => $title) : ?>
-							<?php
-							if ('' === $page) {
-								$page = 'dashboard';
-							}
-							if (!dollie()->in_array_r($page, AccessControl::instance()->get_available_sections())) {
-								continue;
-							}
-							$active_class = $sub_page === $page ? ' dol-text-primary' : 'dol-font-normal dol-text-gray-400 dark:dol-text-gray-300';
-							?>
-							<a class="<?php echo esc_attr($active_class); ?> dol-py-2 dol-px-3 dol-block dol-text-sm dol-text-gray-400 hover:dol-bg-primary hover:dol-text-white" href="<?php echo dollie()->get_site_url($current_id, $page); ?>"><?php echo $title; ?></a>
-						<?php endforeach; ?>
-					</div>
+					<span>
+						<svg class="dol-h-4 dol-w-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+							<path x-show="! open" d="M9 5L16 12L9 19" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path>
+							<path x-show="open" d="M19 9L12 16L5 9" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="display: none;"></path>
+						</svg>
+					</span>
+				</span>
+				<div x-show="open" class="dol-p-2" style="display: none;">
+					<?php foreach ($menu as $page => $title) : ?>
+						<?php
+						if ('' === $page) {
+							$page = 'dashboard';
+						}
+						if (!dollie()->in_array_r($page, AccessControl::instance()->get_available_sections())) {
+							continue;
+						}
+						$active_class = $sub_page === $page ? ' dol-text-primary' : 'dol-font-normal dol-text-gray-400 dark:dol-text-gray-300';
+						?>
+						<a class="<?php echo esc_attr($active_class); ?> dol-py-2 dol-px-3 dol-block dol-text-sm dol-text-gray-400 hover:dol-bg-primary hover:dol-text-white" href="<?php echo dollie()->get_site_url($current_id, $page); ?>"><?php echo $title; ?></a>
+					<?php endforeach; ?>
 				</div>
+			</div>
 		<?php endwhile; ?>
 	</ul>
 <?php endif; ?>
