e24bbdd08f6bc1ccaeff730cec01c0509e56f740
George Tudor <george@seventhqueen.com>
2021-06-14 14:27:45 +0300

    some code refactoring

--- core/Modules/Staging.php
+++ core/Modules/Staging.php
@@ -12,16 +12,18 @@ use Dollie\Core\Log;
 
 /**
  * Class Container
+ *
  * @package Dollie\Core\Modules
  */
 class Staging extends Singleton {
 
 	const LOG_DEPLOY_STARTED = 'wp-staging-deploy-start';
-	const LOG_DEPLOYED = 'wp-staging-deployed';
-	const LOG_DEPLOY_FAILED = 'wp-staging-deploy-failed';
+	const LOG_DEPLOYED       = 'wp-staging-deployed';
+	const LOG_DEPLOY_FAILED  = 'wp-staging-deploy-failed';
+	const LOG_UNDEPLOY       = 'wp-staging-undeploy';
 
 	const OPTION_DATA = '_wpd_staging_data';
-	const OPTION_URL = '_wpd_staging_url';
+	const OPTION_URL  = '_wpd_staging_url';
 
 	/**
 	 * Container constructor.
@@ -30,13 +32,17 @@ class Staging extends Singleton {
 		parent::__construct();
 
 		add_action( 'template_redirect', [ $this, 'staging_change_action' ] );
-
 		add_filter( 'dollie/log/actions', [ $this, 'log_action_filter' ], 10, 2 );
-
 	}
 
+	/**
+	 * Log actions
+	 *
+	 * @param array $actions
+	 * @param array $values
+	 * @return array
+	 */
 	public function log_action_filter( $actions, $values ) {
-
 		$actions[ self::LOG_DEPLOY_STARTED ] = [
 			'title'   => __( 'Staging Site Launch Started', 'dollie' ),
 			'content' => __( sprintf( 'Launching Your Staging Site %s. You\'ll get another notification when it is ready! ', $values[0] ), 'dollie' ),
@@ -44,12 +50,13 @@ class Staging extends Singleton {
 			'link'    => true,
 		];
 
-		$actions[ self::LOG_DEPLOYED ]      = [
+		$actions[ self::LOG_DEPLOYED ] = [
 			'title'   => __( 'Staging Site Launch Completed', 'dollie' ),
 			'content' => __( sprintf( 'Staging Site %s has been successfully launched.', $values[0] ), 'dollie' ),
 			'type'    => 'deploy',
 			'link'    => true,
 		];
+
 		$actions[ self::LOG_DEPLOY_FAILED ] = [
 			'title'   => __( 'Staging Site Launch Failed', 'dollie' ),
 			'content' => __( sprintf( 'Staging Site %s has failed to launch. Please contact our support if the issue persists.', $values[0] ), 'dollie' ),
@@ -58,86 +65,82 @@ class Staging extends Singleton {
 		];
 
 		return $actions;
-
 	}
 
 	/**
 	 * Action to enable/disable staging from front-end
 	 */
 	public function staging_change_action() {
+		if ( ! isset( $_POST['staging_change'] ) || ! wp_verify_nonce( $_POST['_wpnonce'], 'wpd_staging' ) ) {
+			return;
+		}
 
-		if ( isset( $_POST['staging_change'] ) && wp_verify_nonce( $_POST['_wpnonce'], 'wpd_staging' ) ) {
+		if ( ! in_array( $_POST['staging_change'], [ 0, 1 ], false ) ) {
+			return;
+		}
 
-			if ( ! in_array( $_POST['staging_change'], [ 0, 1 ], false ) ) {
+		$status = (int) $_POST['staging_change'];
+
+		// Deploy staging site.
+		if ( 1 === $status ) {
+			// Make sure we can't create staging if limit is reached.
+			if ( dollie()->staging_sites_limit_reached() ) {
 				return;
 			}
-			$status  = (int) $_POST['staging_change'];
-			$user_id = get_current_user_id();
 
-			// deploy staging site
-			if ( $status === 1 ) {
+			$site          = dollie()->get_current_object();
+			$rand          = rand( pow( 10, 2 ), pow( 10, 3 ) - 1 );
+			$domain        = $site->slug . '-' . $rand . '.my-staging-site.xyz';
+			$deploy_status = 'pending';
 
-				// make sure we can't create staging if limit is reached
-				if ( dollie()->staging_sites_limit_reached() ) {
-					return;
-				}
+			$post_body = [
+				'source'  => dollie()->get_container_url( get_the_ID() ),
+				'route'   => $domain,
+				'envVars' => [
+					'S5_DEPLOYMENT_URL' => get_site_url(),
+				],
+			];
 
-				$site          = dollie()->get_current_object();
-				$rand          = rand( pow( 10, 2 ), pow( 10, 3 ) - 1 );
-				$domain        = $site->slug . '-' . $rand . /*'.my-staging-site.xyz'*/
-				                 DOLLIE_DOMAIN;
-				$deploy_status = 'pending';
+			// Send the API request.
+			$request_container_deploy  = Api::post( Api::ROUTE_CONTAINER_STAGING_DEPLOY, $post_body );
+			$response_container_deploy = Api::process_response( $request_container_deploy );
 
-				$post_body = [
-					'source'  => dollie()->get_container_url( get_the_ID() ),
-					'route'   => $domain,
-					'envVars' => [
-						'S5_DEPLOYMENT_URL' => get_site_url(),
-					]
-				];
+			if ( ! $response_container_deploy || ! $response_container_deploy['job'] ) {
+				Log::add_front(
+					self::LOG_DEPLOY_FAILED,
+					dollie()->get_current_object( $site->id ),
+					$domain,
+					print_r( $request_container_deploy, true )
+				);
 
-				// Send the API request.
-				$request_container_deploy  = Api::post( Api::ROUTE_CONTAINER_STAGING_DEPLOY, $post_body );
-				$response_container_deploy = Api::process_response( $request_container_deploy );
-
-				if ( ! $response_container_deploy || ! $response_container_deploy['job'] ) {
-					Log::add_front(
-						self::LOG_DEPLOY_FAILED,
-						dollie()->get_current_object( $site->id ),
-						$domain,
-						print_r( $request_container_deploy, true )
-					);
-
-					$deploy_status = 'failed';
-
-				}
-
-				// set active staging url.
-				update_post_meta( $site->id, self::OPTION_URL, $domain );
-
-				$staging_data = get_post_meta( $site->id, self::OPTION_DATA, true );
-				if ( empty( $staging_data ) ) {
-					$staging_data = [];
-				}
-
-				$staging_data[ $domain ] = [
-					'status' => $deploy_status
-				];
-
-				if ( $deploy_status !== 'failed' ) {
-					$staging_data[ $domain ]['deploy_job'] = $response_container_deploy['job'];
-
-					Log::add_front(
-						self::LOG_DEPLOY_STARTED,
-						dollie()->get_current_object( $site->id ),
-						$domain
-					);
-				}
-
-				update_post_meta( $site->id, self::OPTION_DATA, $staging_data );
+				$deploy_status = 'failed';
 
 			}
 
+			// Set active staging url.
+			update_post_meta( $site->id, self::OPTION_URL, $domain );
+
+			$staging_data = get_post_meta( $site->id, self::OPTION_DATA, true );
+
+			if ( empty( $staging_data ) ) {
+				$staging_data = [];
+			}
+
+			$staging_data[ $domain ] = [
+				'status' => $deploy_status,
+			];
+
+			if ( 'failed' !== $deploy_status ) {
+				$staging_data[ $domain ]['deploy_job'] = $response_container_deploy['job'];
+
+				Log::add_front(
+					self::LOG_DEPLOY_STARTED,
+					dollie()->get_current_object( $site->id ),
+					$domain
+				);
+			}
+
+			update_post_meta( $site->id, self::OPTION_DATA, $staging_data );
 		}
 	}
 
--- templates/widgets/site/pages/staging.php
+++ templates/widgets/site/pages/staging.php
@@ -8,24 +8,20 @@ if ( empty( $staging_url ) ) {
 } else {
 	$status = $staging_data[ $staging_url ]['status'];
 }
-//$status = 'disabled';
 
 $status_title   = __( 'Staging site is disabled', 'dollie' );
 $status_message = sprintf( __( 'Here you can easily manage your staging site for <strong>%s</strong>. Staging allows you to conduct different tests before applying them to your live site.', 'dollie' ), $container->post_name );
 
-if ( $status === 'pending' ) {
-
+if ( 'pending' === $status ) {
 	if ( isset( $staging_data[ $staging_url ]['deploy_job'] ) ) {
 		$job           = $staging_data[ $staging_url ]['deploy_job'];
 		$deploy_status = \Dollie\Core\Modules\Sites\WP::instance()->process_deploy_status( $job );
-
-		// var_dump($deploy_status );
 	}
 
 	$status_title   = __( 'Staging site is being created', 'dollie' );
 	$status_message = sprintf( __( 'Please pe patient while we create a staging site for <strong>%s</strong>.', 'dollie' ), $container->post_name );
 
-} elseif ( $status === 'live' ) {
+} elseif ( 'live' === $status ) {
 	$status_title = sprintf( __( 'Staging site: %', 'dollie' ), $staging_url );
 }
 
@@ -42,63 +38,55 @@ if ( $status === 'pending' ) {
 
 ?>
 
-<?php if ( $status === 'live' ) : ?>
+<?php if ( 'live' === $status ) : ?>
+	<div class="dol-mt-6">
+		<form action="" method="post">
+			<input type="hidden" name="staging_change" value="1">
 
-    <div class="dol-mt-6">
-        <form action="" method="post">
-            <input type="hidden" name="staging_change" value="1">
-
-            <button type="submit" class="dol-bg-red-700" id="staging-form-submit">
+			<button type="submit" class="dol-bg-red-700" id="staging-form-submit">
 				<?php esc_html_e( 'Remove Staging Site', 'dollie' ); ?>
-            </button>
+			</button>
 			<?php wp_nonce_field( 'wpd_staging' ); ?>
-        </form>
-    </div>
-
-<?php elseif( $status === 'disabled' ) : ?>
-
-	<?php
-	if ( dollie()->staging_sites_limit_reached() ) :
-		?>
-        <div class="dol-mt-6">
+		</form>
+	</div>
+<?php elseif ( 'disabled' === $status ) : ?>
+	<?php if ( dollie()->staging_sites_limit_reached() ) : ?>
+		<div class="dol-mt-6">
 			<?php
-			\Dollie\Core\Utils\Tpl::load(
-				'notice',
-				[
-					'icon'  => 'fas fa-exclamation-circle',
-					'type'  => 'error',
-					'title' => esc_html__( 'You have reached your staging sites limit. Please upgrade your subscription!', 'dollie' ),
-				],
-				true
-			);
+				\Dollie\Core\Utils\Tpl::load(
+					'notice',
+					[
+						'icon'  => 'fas fa-exclamation-circle',
+						'type'  => 'error',
+						'title' => esc_html__( 'You have reached your staging sites limit. Please upgrade your subscription!', 'dollie' ),
+					],
+					true
+				);
 
-			return;
+				return;
 			?>
-        </div>
-
+		</div>
 	<?php endif; ?>
-
-
 <?php endif; ?>
 
 
 <div class="dol-mt-6">
-    <form action="" method="post">
-        <input type="hidden" name="staging_change" value="1">
+	<form action="" method="post">
+		<input type="hidden" name="staging_change" value="1">
 
-        <button type="submit" id="staging-form-submit">
+		<button type="submit" id="staging-form-submit">
 			<?php esc_html_e( 'Create a Staging Site', 'dollie' ); ?>
-        </button>
+		</button>
 		<?php wp_nonce_field( 'wpd_staging' ); ?>
-    </form>
+	</form>
 </div>
 
 <script>
-    jQuery(document).ready(function ($) {
-        $('#staging-form-submit').on('click', function (e) {
-            $(this).attr('disabled', true);
+	jQuery(document).ready(function ($) {
+		$('#staging-form-submit').on('click', function (e) {
+			$(this).attr('disabled', true);
 
-            $(this).parent('form').submit();
-        });
-    });
+			$(this).parent('form').submit();
+		});
+	});
 </script>
