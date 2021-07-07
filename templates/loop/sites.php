a14889863ecfe80fec2a23a6cfd842331ab7576e
George Tudor <george@seventhqueen.com>
2021-07-07 10:57:28 +0300

    filters popup

--- templates/loop/sites.php
+++ templates/loop/sites.php
@@ -54,6 +54,44 @@ $actions = [
 	</div>
 </div>
 
+<div id="dol-modal-id-filters" class="dol-modal">
+	<div class="dol-flex dol-justify-center dol-items-center dol-w-full dol-h-full">
+		<div class="dol-bg-white dol-rounded dol-w-2/6 dol-p-4">
+			<div class="dol-mb-4">
+				<div class="dol-flex dol-justify-between dol-items-center">
+					<div class="dol-font-bold dol-text-2xl">
+						<?php esc_html_e( 'Filters', 'dollie' ); ?>
+					</div>
+					<div class="">
+						<span class="dol-modal-close dol-cursor-pointer"><i class="fas fa-times"></i></span>
+					</div>
+				</div>
+			</div>
+
+			<div class="">
+				<div class="">
+					<label for="per-page" class="dol-font-bold dol-uppercase dol-mb-1 dol-text-xs dol-text-gray-600"><?php esc_html_e( 'Per page', 'dollie' ); ?></label>
+					<select id="per-page" class="dol-w-20 dol-bg-white dol-rounded dol-px-3 dol-py-1 dol-border-solid dol-border-gray-300 dol-text-sm dol-text-gray-700 focus:dol-border-gray-400 focus:dol-outline-none">
+						<option value="" disabled><?php esc_html_e( 'View', 'dollie' ); ?></option>
+						<option value="10" selected>10</option>
+						<option value="20">20</option>
+						<option value="30">30</option>
+						<option value="40">40</option>
+						<option value="50">50</option>
+					</select>
+				</div>
+			</div>
+
+			<div class="dol-mt-4">
+				<button type="button" class="dol-apply-filters dol-px-4 dol-py-2 dol-bg-primary-500 hover:dol-bg-primary-600 dol-border-0 dol-rounded dol-text-white dol-text-sm focus:dol-outline-none focus:dol-bg-primary-600">
+					<?php esc_html_e( 'Apply', 'dollie' ); ?>
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 <div class="dol-mb-6 dol-widget-site-search">
 	<div class="dol-rounded dol-p-4 dol-bg-white dol-shadow-md">
 		<div class="dol-flex dol-flex-wrap dol-items-center md:dol-justify-between">
@@ -88,14 +126,9 @@ $actions = [
 				</div>
 
 				<div class="dol-mx-3 dol-inline-block">
-					<select class="dol-w-20 dol-bg-white dol-rounded dol-px-4 dol-py-2 dol-border-solid dol-border-gray-300 dol-text-base dol-text-gray-700 focus:dol-border-gray-400 focus:dol-outline-none">
-						<option value="" disabled><?php esc_html_e( 'View', 'dollie' ); ?></option>
-						<option value="10" selected>10</option>
-						<option value="20">20</option>
-						<option value="30">30</option>
-						<option value="40">40</option>
-						<option value="50">50</option>
-					</select>
+					<span data-modal-id="dol-modal-id-filters" class="dol-open-modal dol-block dol-p-3 dol-m-0 dol-bg-gray-200 hover:dol-bg-gray-300 dol-text-gray-700 dol-flex dol-items-center dol-cursor-pointer">
+						<i class="fas fa-filter"></i>
+					</span>
 				</div>
 
 				<div class="dol-inline-flex dol-items-center dol-rounded dol-overflow-hidden dol-mb-4 md:dol-mb-0 dol-h-10 md:dol-h-auto">
