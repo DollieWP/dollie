e19261078756182a2e4ca179cccb61dd976d8b2f
Gabriel Fratica <gabriel@seventhqueen.com>
2021-08-26 13:16:25 +0300

    possible acf fix

--- core/Modules/Container.php
+++ core/Modules/Container.php
@@ -33,7 +33,7 @@ class Container extends Singleton {
 		add_action( 'init', [ $this, 'check_deployment_domain_status' ] );
 
 		add_filter( 'query_vars', [ $this, 'query_vars' ] );
-		add_action( 'single_template', [ $this, 'add_acf_form_head' ], 9 );
+		add_action( 'wp', [ $this, 'add_acf_form_head' ], 9 );
 		add_filter( 'single_template', [ $this, 'container_template' ] );
 
 		add_action( 'template_redirect', [ $this, 'remove_customer_domain_action' ] );
@@ -222,7 +222,7 @@ class Container extends Singleton {
 	 * @return void
 	 */
 	public function add_acf_form_head() {
-		if ( in_array( get_query_var( 'sub_page' ), [ 'blueprints' ] ) ) {
+		if ( get_query_var( 'sub_page' ) === 'blueprints' ) {
 			acf_form_head();
 		}
 	}
