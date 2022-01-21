b912daf5602005143be6d4c94220ef63b6d4bd56
George Tudor <georgebitq@gmail.com>
2022-01-21 17:22:21 +0200

    domain connect ui fixes

--- templates/widgets/site/pages/domains.php
+++ templates/widgets/site/pages/domains.php
@@ -12,7 +12,7 @@ $domain_wizard_complete = get_post_meta( get_the_ID(), 'wpd_domain_migration_com
 
 <?php if ( 'pending' === $dns_manager ) : ?>
 
-    <div class="dol-my-6">
+	<div class="dol-my-6">
 		<?php
 		dollie()->load_template(
 			'notice',
@@ -25,36 +25,56 @@ $domain_wizard_complete = get_post_meta( get_the_ID(), 'wpd_domain_migration_com
 			true
 		);
 		?>
-    </div>
+
+		<div class="dol-rounded dol-overflow-hidden dol-shadow dol-mt-6">
+			<div class="dol-p-4 lg:dol-px-8 lg:dol-py-6">
+				<h4 class="dol-text-lg dol-font-bold"><?php esc_html_e( 'Your domain will get validated once we detect the following nameservers attached to it.', 'dollie' ); ?></h4>
+				<ul class="dol-m-0">
+					<li>pdns1.stratus5.com</li>
+					<li>pdns2.stratus5.com</li>
+					<li>pdns3.stratus5.com</li>
+				</ul>
+				<span class="dol-block dol-mb-4 dol-mt-6"><?php esc_html_e( 'Want to change the domain or have you typed in the wrong domain name? You can cancel at any time and try again!', 'dollie' ); ?></span>
+				<form action="<?php echo get_permalink( get_the_ID() ); ?>?remove-domain=<?php echo get_post_meta( get_the_ID(), 'wpd_container_id', true ); ?>"
+					method="post">
+					<button name="remove_customer_dns" id="remove_customer_dns" type="submit"
+							class="dol-px-4 dol-py-2 dol-bg-red-600 dol-text-white dol-rounded">
+						<i class="fa fa-trash-o" aria-hidden="true"></i>
+						<?php esc_html_e( 'Cancel', 'dollie' ); ?>
+					</button>
+				</form>
+			</div>
+		</div>
+	</div>
 
 <?php elseif ( ! empty( $domain ) ) : ?>
 
 	<?php if ( 'no' === $domain_wizard_complete ) : ?>
 
-        <div class="dol-my-6">
-            <div class="dol-rounded dol-overflow-hidden dol-shadow dol-mb-6">
-                <div class="dol-p-4 lg:dol-px-8 lg:dol-py-4 dol-bg-gray-200">
-                    <h4 class="dol-p-0 dol-m-0 dol-font-bold  dol-text-base md:dol-text-xl">
+		<div class="dol-my-6">
+			<div class="dol-rounded dol-overflow-hidden dol-shadow dol-mb-6">
+				<div class="dol-p-4 lg:dol-px-8 lg:dol-py-4 dol-bg-gray-200">
+					<h4 class="dol-p-0 dol-m-0 dol-font-bold  dol-text-base md:dol-text-xl">
 						<?php esc_html_e( 'Site URL replacement is not complete ', 'dollie' ); ?>
-                    </h4>
-                </div>
-                <div class="dol-p-4 lg:dol-px-8 lg:dol-py-6">
+					</h4>
+				</div>
+				<div class="dol-p-4 lg:dol-px-8 lg:dol-py-6">
 					<span class="dol-block dol-mb-4">
 						<?php printf( __( 'Your domain <strong>%s</strong> is connected but the URL replacement wasn\'t complete.', 'dollie' ), $domain ); ?>
 					</span>
 
-                    <a class="dol-px-4 dol-py-2 dol-bg-red-600 dol-text-white dol-rounded"
-                       href="<?php echo get_permalink( get_the_ID() ); ?>?update-domain-url">
-                        <i class="fa fa-refresh" aria-hidden="true"></i>
+					<a class="dol-px-4 dol-py-2 dol-bg-red-600 dol-text-white dol-rounded"
+					   href="<?php echo get_permalink( get_the_ID() ); ?>?update-domain-url">
+						<i class="fa fa-refresh" aria-hidden="true"></i>
 						<?php esc_html_e( 'Replace URL now', 'dollie' ); ?>
-                    </a>
-                </div>
-            </div>
-        </div>
+					</a>
+				</div>
+			</div>
+		</div>
 
 	<?php else : ?>
 
-        <div class="dol-my-6">
+		<div class="dol-my-6">
 			<?php
 			dollie()->load_template(
 				'notice',
@@ -66,7 +86,7 @@ $domain_wizard_complete = get_post_meta( get_the_ID(), 'wpd_domain_migration_com
 				true
 			);
 			?>
-        </div>
+		</div>
 
 		<?php
 		dollie()->load_template(
@@ -83,26 +103,26 @@ $domain_wizard_complete = get_post_meta( get_the_ID(), 'wpd_domain_migration_com
 
 <?php endif; ?>
 
-<?php if ( ! empty( $domain ) || 'pending' === $dns_manager ) : ?>
-    <div class="dol-rounded dol-overflow-hidden dol-shadow dol-mb-6">
-        <div class="dol-p-4 lg:dol-px-8 lg:dol-py-4 dol-bg-gray-200">
-            <h4 class="dol-p-0 dol-m-0 dol-font-bold dol-text-base md:dol-text-xl">
+<?php if ( ! empty( $domain ) ) : ?>
+	<div class="dol-rounded dol-overflow-hidden dol-shadow dol-mb-6">
+		<div class="dol-p-4 lg:dol-px-8 lg:dol-py-4 dol-bg-gray-200">
+			<h4 class="dol-p-0 dol-m-0 dol-font-bold dol-text-base md:dol-text-xl">
 				<?php esc_html_e( 'Remove your linked domain', 'dollie' ); ?>
-            </h4>
-        </div>
-        <div class="dol-p-4 lg:dol-px-8 lg:dol-py-6">
-            <span class="dol-block dol-mb-4"><?php esc_html_e( 'Have you changed your domain name? You can unlink your current domain by pressing the button below. Once you have removed your current domain you can add your new domain.', 'dollie' ); ?></span>
-            <form action="<?php echo get_permalink( get_the_ID() ); ?>?remove-domain=<?php echo get_post_meta( get_the_ID(), 'wpd_container_id', true ); ?>"
-                  method="post">
-                <button name="remove_customer_domain" id="remove_customer_domain" type="submit"
-                        class="dol-px-4 dol-py-2 dol-bg-red-600 dol-text-white dol-rounded">
-                    <i class="fa fa-trash-o" aria-hidden="true"></i>
+			</h4>
+		</div>
+		<div class="dol-p-4 lg:dol-px-8 lg:dol-py-6">
+			<span class="dol-block dol-mb-4"><?php esc_html_e( 'Have you changed your domain name? You can unlink your current domain by pressing the button below. Once you have removed your current domain you can add your new domain.', 'dollie' ); ?></span>
+			<form action="<?php echo get_permalink( get_the_ID() ); ?>?remove-domain=<?php echo get_post_meta( get_the_ID(), 'wpd_container_id', true ); ?>"
+				  method="post">
+				<button name="remove_customer_domain" id="remove_customer_domain" type="submit"
+						class="dol-px-4 dol-py-2 dol-bg-red-600 dol-text-white dol-rounded">
+					<i class="fa fa-trash-o" aria-hidden="true"></i>
 					<?php esc_html_e( 'Remove Domain', 'dollie' ); ?>
-                </button>
-            </form>
-        </div>
-    </div>
-    <p class="dol-mt-2 dol-font-bold">
+				</button>
+			</form>
+		</div>
+	</div>
+	<p class="dol-mt-2 dol-font-bold">
 		<?php esc_html_e( 'Your current domain IP information:', 'dollie' ); ?></p>
 	<?php
 	dollie()->load_template(
@@ -117,7 +137,7 @@ $domain_wizard_complete = get_post_meta( get_the_ID(), 'wpd_domain_migration_com
 <?php endif; ?>
 
 <?php if ( ! \Dollie\Core\Modules\Forms\DomainConnect::instance()->is_form_restricted() ) : ?>
-    <div class="dol-mt-6">
+	<div class="dol-mt-6">
 		<?php echo do_shortcode( '[dollie_form form="form_dollie_domain_connect"]' ); ?>
-    </div>
+	</div>
 <?php endif; ?>
