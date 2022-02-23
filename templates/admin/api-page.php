c10b3c1521ee07363dc7556b3910c951f1eeb2c3
Bowe Frankema <bowe@cfcommunity.net>
2022-02-23 14:45:21 +0100

    Clean up API details template
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- templates/admin/api-page.php
+++ templates/admin/api-page.php
@@ -5,35 +5,32 @@ use Dollie\Core\Utils\Api;
 ?>
 
 <div class="wrap">
-	<h1><span class="dashicons dashicons-rest-api"></span> Dollie API</h1>
 
-	<?php if ( isset( $_GET['err'] ) ) : ?>
-		<div class="notice dollie-notice">
-			<?php esc_html_e( 'Something went wrong. Please try again later or contact our support.', 'dollie' ); ?>
+	<?php if (isset($_GET['err'])) : ?>
+		<div class="notice dollie-notice dollie-error">
+			<h4>Sorry, We could not connect this site to our API</h4>
+			<?php esc_html_e('Could you please email us directly at support@getdollie.com and we will get this fixed immediately.', 'dollie'); ?>
 		</div>
 	<?php endif; ?>
 
-	<?php if ( isset( $_GET['status'] ) && $_GET['status'] === 'not_connected' ) : ?>
-		<div class="notice dollie-notice">
-			<?php esc_html_e( 'You are not connected with the Dollie API. Please follow the instruction at the top of the page regarding API authentication.', 'dollie' ); ?>
-		</div>
-	<?php endif; ?>
 
-	<?php if ( Api::get_auth_token() ) : ?>
+	<?php if (Api::get_auth_token()) : ?>
 
-		<div class="notice dollie-notice">
-			<h3><?php esc_html_e( 'Your site is connected to Dollie', 'dollie' ); ?></h3>
-		</div>
+		<p><?php esc_html_e('Here is the token that links your site and partner account to the Dollie API.', 'dollie'); ?></p>
 
-		<h3>API TOKEN</h3>
-		<p><?php esc_html_e( 'This is the token that links your site and partner account to the Dollie API.', 'dollie' ); ?></p>
+		<pre class="dol-p-2 dol-bg-gray-200 dol-block"><?php echo esc_attr(Api::get_auth_token()); ?></pre>
 
-		<input style="width: 400px" type="text" readonly
-			   value="<?php echo esc_attr( Api::get_auth_token() ); ?>">
-		<a href="<?php echo esc_url( add_query_arg( 'disconnect_dollie', 1 ) ); ?>" class="button button-large">
-			<?php esc_html_e( 'Disconnect from Dollie', 'dollie' ); ?>
+		<a href="<?php echo esc_url(add_query_arg('disconnect_dollie', 1)); ?>" class="button">
+			<?php esc_html_e('Disconnect from Dollie', 'dollie'); ?>
 		</a>
-		<p><?php esc_html_e( 'If you disconnect your site from Dollie, you won\'t be able to deploy sites or interact with our servers.', 'dollie' ); ?></p>
+		<p class="dol-text-xs dol-font-bold"><?php esc_html_e('If you disconnect your site from Dollie, it would mean that your platform would stop working.', 'dollie'); ?></p>
+
+	<?php endif; ?>
+
+
+	<?php if (!Api::get_auth_token()) : ?>
+
+		<?php esc_html_e('You are not connected with the Dollie API. Please follow the instruction at the top of the page regarding API authentication.', 'dollie'); ?>
 
 	<?php endif; ?>
 
