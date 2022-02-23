f3895702082f3fbbfbdf39ed323f0d049093a721
Bowe Frankema <bowe@cfcommunity.net>
2022-02-23 14:44:18 +0100

    Use standard option check instead of get field
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Modules/AccessControl.php
+++ core/Modules/AccessControl.php
@@ -175,9 +175,9 @@ class AccessControl extends Singleton {
 		];
 
 		// Get role settings.
-		$settings_view   = get_field( 'wpd_view_sites_permission', 'options' );
-		$settings_update = get_field( 'manage_sites_permission', 'options' );
-		$settings_delete = get_field( 'delete_sites_permission', 'options' );
+		$settings_view   = get_option( 'options_wpd_view_sites_permission' );
+		$settings_update = get_option( 'options_manage_sites_permission' );
+		$settings_delete = get_option( 'option_sdelete_sites_permission' );
 
 		if ( empty( $settings_view ) ) {
 			$settings_view = [];
