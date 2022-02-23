b8232f1d75ca63ec61d910709d871501cb4567f0
Bowe Frankema <bowe@cfcommunity.net>
2022-02-23 15:37:18 +0100

    Set up default import package
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Extras/dollie-setup/includes/agency/defaults/setup.php
+++ core/Extras/dollie-setup/includes/agency/defaults/setup.php
@@ -9,28 +9,21 @@ function ocdi_import_files_string()
 			// 'import_customizer_file_url' => 'http://www.your_domain.com/ocdi/customizer.dat',
 			'import_preview_image_url'   => 'http://www.your_domain.com/ocdi/preview_import_image1.jpg',
 			'preview_url'                => 'http://www.your_domain.com/my-demo-1',
-		],
-		[
-			'import_file_name'           => 'Demo Import 2',
-			'categories'                 => ['New category', 'Old category'],
-			'import_file_url'            => 'http://www.your_domain.com/ocdi/demo-content2.xml',
-			'import_widget_file_url'     => 'http://www.your_domain.com/ocdi/widgets2.json',
-			'import_customizer_file_url' => 'http://www.your_domain.com/ocdi/customizer2.dat',
-			'import_redux'               => [
-				[
-					'file_url'    => 'http://www.your_domain.com/ocdi/redux.json',
-					'option_name' => 'redux_option_name',
-				],
-				[
-					'file_url'    => 'http://www.your_domain.com/ocdi/redux2.json',
-					'option_name' => 'redux_option_name_2',
-				],
-			],
-			'import_preview_image_url'   => 'http://www.your_domain.com/ocdi/preview_import_image2.jpg',
-			'preview_url'                => 'http://www.your_domain.com/my-demo-2',
-		],
+		]
 	];
 }
 add_filter('ocdi/import_files', 'ocdi_import_files_string');
+
+function ocdi_plugin_page_setup($default_settings)
+{
+	$default_settings['parent_slug'] = 'admin.php?page=dollie_setup';
+	$default_settings['page_title']  = esc_html__('Setup Wizard', 'one-click-demo-import');
+	$default_settings['menu_title']  = esc_html__('Setup Your Agency Platform', 'one-click-demo-import');
+	$default_settings['capability']  = 'import';
+	$default_settings['menu_slug']   = 'dollie-setup-wizard';
+
+	return $default_settings;
+}
+add_filter('ocdi/plugin_page_setup', 'ocdi_plugin_page_setup');
 
 
