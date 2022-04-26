0efbba08594b8bfc3d625cdf712d46bf358e1368
Bowe Frankema <bowe@cfcommunity.net>
2022-04-26 16:37:07 +0200

    Demo setup
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Extras/dollie-setup/admin/templates/base/package-details.php
+++ core/Extras/dollie-setup/admin/templates/base/package-details.php
@@ -2,11 +2,12 @@
 <?php if ( dollie_setup_get_theme_prop( 'name', $_GET['dollie_setup-package-details'] ) ) : ?>
 
 
+
 	<h2><?php esc_html_e( 'Dollie Agency Setup', 'dollie-setup' ); ?></h2>
 	<?php dollie_setup_get_template_part( 'package-details-theme', $_GET['dollie_setup-package-details'] ); ?>
+	<?php dollie_setup_get_template_part( 'package-onboarding-form' ); ?>
 
 <?php endif; ?>
 
-<h3><?php esc_html_e( 'Plugins', 'dollie-setup' ); ?></h3>
 <?php // dollie_setup_get_template_part( 'package-details-plugins', $_GET['dollie_setup-package-details'] ); ?>
 <?php // dollie_setup_get_template_part( 'wrapper-footer' ); ?>
---
+++ core/Extras/dollie-setup/admin/templates/base/package-onboarding-form.php
@@ -0,0 +1,8 @@
+<p class="dol-mb-5"><?php printf( esc_html__( 'With Dollie you can create a powerful platform for your Agency, that can be used by your team and your clients. Ready to get started?', 'dollie-setup' ), dollie_setup_get_package_prop( 'name', $_GET['dollie_setup-package-details'] ) ); ?></p>
+
+<div class="dol-mt-5 dol-p-0">
+	<?php
+	// echo do_shortcode( '[dollie_form form="form_dollie_' . $_GET['dollie_setup-package-details'] . '_onboarding" ]' )
+	echo do_shortcode( '[dollie_form form="form_626802e1966b3" ]' )
+	?>
+</div>
