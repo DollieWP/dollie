846e62e6f72eeab7e7fa8c07de2d731f762051a6
Bowe Frankema <bowe@cfcommunity.net>
2022-05-05 10:49:32 +0200

    Add custom domain to setup list
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Extras/dollie-setup/admin/templates/base/setup-complete.php
+++ core/Extras/dollie-setup/admin/templates/base/setup-complete.php
@@ -154,7 +154,7 @@
 							</div>
 						</div>
 						<div>
-							<!-- Heroicon name: solid/chevron-right -->
+						<!-- Heroicon name: solid/chevron-right -->
 							<svg class="dol-h-5 dol-w-5 dol-text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 								<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
 							</svg>
@@ -164,6 +164,92 @@
 			</li>
 
 		<?php } ?>
+
+
+	<?php if ( get_option( 'wpd_deployment_domain' ) && get_option( 'wpd_deployment_domain_status' ) ) : ?>
+			<li>
+				<a href="#" class="dol-block hover:dol-bg-gray-50">
+					<div class="dol-flex dol-items-center dol-px-4 dol-py-4 sm:dol-px-6">
+						<div class="dol-min-w-0 dol-flex-1 dol-flex dol-items-center">
+							<div class="dol-flex-shrink-0">
+								<svg xmlns="http://www.w3.org/2000/svg" class="dol-h-10 dol-w-10 dol-rounded-full dol-bg-green-400 dol-p-2 dol-text-green-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+								<path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+								</svg>
+							</div>
+							<div class="dol-min-w-0 dol-flex-1 dol-px-4 md:dol-grid md:dol-grid-cols-2 md:dol-gap-4">
+								<div>
+									<p class="dol-text-sm dol-font-medium dol-text-green-600 dol-truncate"><?php esc_html_e( 'Your Custom Domain is Active', 'dollie' ); ?></p>
+									<p class="dol-mt-2 dol-flex dol-items-center dol-text-sm dol-text-gray-500">
+										<!-- Heroicon name: solid/mail -->
+										<span class="dol-truncate">You are using the domain <?php echo get_option( 'wpd_deployment_domain' ); ?>. </span>
+									</p>
+								</div>
+								<div class="dol-hidden dol-mt-3 md:dol-block">
+									<div>
+										<p class="dol-mt-2 dol-flex dol-items-center dol-text-sm dol-text-gray-500">
+											<!-- Heroicon name: solid/check-circle -->
+											<svg class="dol-flex-shrink-0 dol-mr-1.5 dol-h-5 dol-w-5 dol-text-green-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+												<path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd" />
+											</svg>
+											Completed
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div>
+						<!-- Heroicon name: solid/chevron-right -->
+							<svg class="dol-h-5 dol-w-5 dol-text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+								<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</li>
+		<?php
+		else :
+			?>
+					<li>
+				<a href="<?php echo get_admin_url(); ?>admin.php?page=wpd_platform_setup" class="dol-block hover:dol-bg-gray-50">
+					<div class="dol-flex dol-items-center dol-px-4 dol-py-4 sm:dol-px-6">
+						<div class="dol-min-w-0 dol-flex-1 dol-flex dol-items-center">
+							<div class="dol-flex-shrink-0">
+								<svg xmlns="http://www.w3.org/2000/svg" class="dol-h-10 dol-w-10 dol-rounded-full dol-bg-red-400 dol-p-2 dol-text-red-100" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+</svg>
+							</div>
+							<div class="dol-min-w-0 dol-flex-1 dol-px-4 md:dol-grid md:dol-grid-cols-2 md:dol-gap-4">
+								<div>
+									<p class="dol-text-sm dol-font-medium dol-text-red-600 dol-truncate"><?php esc_html_e( 'Custom Domain Setup', 'dollie' ); ?></p>
+									<p class="dol-mt-2 dol-flex dol-items-center dol-text-sm dol-text-gray-500">
+										<!-- Heroicon name: solid/mail -->
+										<span class="dol-truncate">You have not connected your own domain yet.</span>
+									</p>
+								</div>
+								<div class="dol-hidden dol-mt-3 md:dol-block">
+									<div>
+										<p class="dol-mt-2 dol-flex dol-items-center dol-text-sm dol-text-gray-500">
+
+								<!-- Heroicon name: solid/chevron-right -->
+							<svg class="dol-h-5 dol-w-5 dol-text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+								<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+							</svg>
+											Not Completed
+										</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div>
+						<!-- Heroicon name: solid/chevron-right -->
+							<svg class="dol-h-5 dol-w-5 dol-text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+								<path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+							</svg>
+						</div>
+					</div>
+				</a>
+			</li>
+		<?php endif; ?>
 
 
 	</ul>
