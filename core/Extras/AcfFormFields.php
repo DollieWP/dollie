323eab492f7505ff762e28fa25e33a0473e45aad
Bowe Frankema <bowe@cfcommunity.net>
2022-05-16 19:57:47 +0200

    Use correct form key/name in ACF field
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Extras/AcfFormFields.php
+++ core/Extras/AcfFormFields.php
@@ -1082,7 +1082,7 @@ Whilst PHP7 gives your site a big performance boost not all plugins and themes a
 					array(
 						'param'    => 'af_form',
 						'operator' => '==',
-						'value'    => 'form_626802e1966b3',
+						'value'    => 'form_dollie_agency_onboarding',
 					),
 				),
 			),
