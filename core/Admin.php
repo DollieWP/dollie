461be389b27af29cb89b13c613fbe50bceb54b27
Bowe Frankema <bowe@cfcommunity.net>
2022-05-19 09:31:59 +0200

    Remove unneeded partner status from menu title
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Admin.php
+++ core/Admin.php
@@ -91,17 +91,17 @@ final class Admin extends Singleton implements ConstInterface {
 			return;
 		}
 
-		if ( dollie()->get_partner_status() == 'trial' ) {
-			$status = '<span class="dol-status dol-staging">Trial<span>';
-		} elseif ( dollie()->get_partner_status() == 'staging' ) {
-		   $status = '<span class="dol-status dol-staging">Live<span>';
-		} else {
-			$status = '<span class="dol-status dol-live">Live<span>';
-		}
+		// if ( dollie()->get_partner_status() == 'trial' ) {
+		// 	$status = '<span class="dol-status dol-staging">Trial<span>';
+		// } elseif ( dollie()->get_partner_status() == 'staging' ) {
+		//    $status = '<span class="dol-status dol-staging">Live<span>';
+		// } else {
+		// 	$status = '<span class="dol-status dol-live">Live<span>';
+		// }
 
 		$args = array(
-			'page_title'  => __( 'Settings' . $status, 'dollie' ),
-			'menu_title'  => __( 'Settings' . $status, 'dollie' ),
+			'page_title'  => __( 'Settings', 'dollie' ),
+			'menu_title'  => __( 'Settings', 'dollie' ),
 			'menu_slug'   => self::PANEL_SLUG,
 			'capability'  => 'manage_options',
 			'position'    => '99',
