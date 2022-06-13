72933c42a05d48d0ab893558c4505e2c63750ddb
Bowe Frankema <bowe@cfcommunity.net>
2022-06-13 12:01:41 +0200

    Add back active theme design
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- templates/widgets/site/pages/themes.php
+++ templates/widgets/site/pages/themes.php
@@ -10,6 +10,44 @@ if ( ! isset( $container ) ) {
 	<?php esc_html_e( 'Themes', 'dollie' ); ?>
 </h2>
 
+<h2 class="dol-text-gray-500 text-s dol-font-small dol-uppercase dol-tracking-wide dol-mb-5 dol-text-xl">
+		<?php esc_html_e( 'Active Theme', 'dollie' ); ?>
+	</h2>
+
+	<div class="dol-rounded dol-overflow-hidden dol-border <?php do_action( 'dol_add_widget_classes' ); ?> dol-mb-12">
+		<div class="dol-p-4 lg:dol-px-8 lg:dol-py-4 dol-bg-primary-600 dol-border-0 dol-border-b">
+			<div class="dol-flex dol-flex-wrap">
+				<div class="dol-w-full lg:dol-w-3/6">
+					<h4 class="dol-m-0 dol-p-0 dol-font-bold dol-text-base md:dol-text-xl dol-text-white">
+						<?php echo esc_html( $theme['name'] ); ?>
+						<span class="dol-text-sm">
+							<?php echo esc_html( $theme['name'] ); ?>
+						</span>
+					</h4>
+				</div>
+				<div class="dol-w-full lg:dol-w-3/6 lg:dol-text-right dol-mt-1 lg:dol-mt-0">
+					<a class="dol-text-sm dol-text-primary-200 hover:dol-text-primary-300" target="_blank" href="<?php echo esc_url( $data['site_data']['Theme AuthorURI'] ); ?>">
+						<i class="fas fa-user dol-text-primary-200 dol-mr-2"></i>
+						<?php esc_html_e( 'Developed by', 'dollie' ); ?>
+						<?php echo esc_html( $data['site_data']['Theme Author'] ); ?>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="dol-p-4 lg:dol-px-8 lg:dol-py-6 ">
+			<div class="dol-flex dol-flex-wrap dol--mx-4 dol-my-2">
+				<div class="dol-w-full md:dol-w-2/5 lg:dol-w-1/3 dol-px-4">
+					<div class="dol-border dol-border-solid dol-border-primary-50 dol-rounded dol-overflow-hidden">
+						<img class="dol-block" src="<?php echo esc_url( get_post_meta( get_the_ID(), 'wpd_installation_site_theme_screenshot', true ) ); ?>" alt="<?php echo esc_attr( $data['container_details']['Name'] ); ?>">
+					</div>
+				</div>
+				<div class="dol-w-full md:dol-w-3/5 lg:dol-w-2/3 dol-px-4 dol-mt-4 md:dol-mt-0 dol-text-sm md:dol-text-base">
+					<span><?php echo esc_html( $data['site_data']['Theme Description'] ); ?></span>
+				</div>
+			</div>
+		</div>
+	</div>
+
 <div class="dol-flex dol-flex-wrap dol-border <?php do_action( 'dol_add_widget_classes' ); ?> dol-overflow-hidden">
 	<div class="dol-hidden dol-w-full md:dol-flex dol-flex-wrap dol-items-center dol-bg-primary-600 dol-p-4 lg:dol-px-8 dol-text-white">
 		<div class="dol-w-5/12 lg:dol-w-6/12 dol-text-left">
@@ -26,7 +64,11 @@ if ( ! isset( $container ) ) {
 		</div>
 	</div>
 	<div class="dol-w-full dol-overflow-hidden">
-		<?php foreach ( $container->get_themes() as $theme ) : ?>
+
+		<?php
+		print("<pre>".print_r($container->get_details(),true)."</pre>");
+		foreach (
+			$container->get_themes() as $theme ) : ?>
 			<div class="dol-w-full dol-flex dol-flex-wrap dol-items-center dol-p-4 lg:dol-px-8 dol-border-0 dol-border-b last:dol-border-0 dol-relative dol-group hover:dol-bg-ash-100">
 				<div class="dol-absolute dol-w-2 dol-h-full dol-bg-gray-200 dol-left-0 dol-top-0 dol-hidden md:group-hover:dol-block"></div>
 				<div class="dol-w-full dol-mb-4 md:dol-mb-0 md:dol-w-5/12 lg:dol-w-6/12">
@@ -64,7 +106,7 @@ if ( ! isset( $container ) ) {
 					<?php echo esc_html( $theme['version'] ); ?>
 				</div>
 				<div class="dol-w-4/12 md:dol-w-3/12 lg:dol-w-2/12 dol-text-right">
-					<a class="dol-inline-block md:dol-px-4 md:dol-py-2 md:dol-bg-secondary dol-text-white md:dol-text-white dol-text-sm md:hover:dol-text-white md:hover:dol-bg-secondary-600 dol-rounded" 
+					<a class="dol-inline-block md:dol-px-4 md:dol-py-2 md:dol-bg-secondary dol-text-white md:dol-text-white dol-text-sm md:hover:dol-text-white md:hover:dol-bg-secondary-600 dol-rounded"
 						href="<?php echo esc_url( $theme['uri'] ); ?>" target="_blank">
 						<?php echo dollie()->icon()->arrow_right(); ?>
 						<span class="md:dol-hidden lg:dol-inline-block"><?php esc_html_e( 'Visit', 'dollie' ); ?></span>
