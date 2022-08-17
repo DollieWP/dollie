a46a564e55a0fbea867651ae459ea27463755e7a
Bowe Frankema <bowe@cfcommunity.net>
2022-08-17 14:53:18 +0200

    Add VIP to Blueprint selection
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- templates/parts/blueprint-image.php
+++ templates/parts/blueprint-image.php
@@ -6,8 +6,15 @@ if ( ! isset( $container ) || ! $container->is_blueprint() ) {
 
 ?>
 
-<img data-toggle="tooltip" 
-	data-placement="bottom" 
+<img data-toggle="tooltip"
+	data-placement="bottom"
 	data-tooltip="<?php echo esc_attr( $container->get_saved_description() ); ?>"
 	class="fw-blueprint-screenshot acf__tooltip" src="<?php echo esc_url( $container->get_screenshot() ); ?>">
 <?php echo esc_html( $container->get_saved_title() ); ?>
+		<?php if ( $container->is_vip() ) : ?>
+			<span data-toggle="tooltip"
+	data-placement="bottom"
+	data-tooltip="This Blueprint is only available for VIPs">
+			<?php echo dollie()->icon()->vip( 'dol-text-secondary dol-text-s' ); ?>
+		</span>
+		<?php endif; ?>
