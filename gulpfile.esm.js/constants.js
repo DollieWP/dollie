d32f699b57826cab3c63278ff58ea21667112af5
George Tudor <georgebitq@gmail.com>
2022-10-18 17:19:41 +0300

    upgrade gulp-sass and install node-sass

--- gulpfile.esm.js/constants.js
+++ gulpfile.esm.js/constants.js
@@ -6,35 +6,35 @@ export const rootPath = process.env.INIT_CWD;
 
 // PHPCS options
 export const PHPCSOptions = {
-  bin: `${rootPath}/vendor/bin/phpcs`,
-  standard: `${rootPath}/phpcs.xml.dist`,
-  warningSeverity: 0,
+    bin: `${rootPath}/vendor/bin/phpcs`,
+    standard: `${rootPath}/phpcs.xml.dist`,
+    warningSeverity: 0,
 };
 
 export const names = {
-  PHPNamespace: "Dollie",
-  slug: "dollie",
-  name: "Dollie",
-  underscoreCase: "dollie",
-  constant: "DOLLIE",
-  camelCase: "Dollie",
-  camelCaseVar: "dollie",
-  author: "Dollie",
+    PHPNamespace: "Dollie",
+    slug: "dollie",
+    name: "Dollie",
+    underscoreCase: "dollie",
+    constant: "DOLLIE",
+    camelCase: "Dollie",
+    camelCaseVar: "dollie",
+    author: "Dollie",
 };
 
 // Project paths
 let paths = {
-  languages: {
-    src: [
-      `${rootPath}/**/*.php`,
-      `!${rootPath}/optional/**/*.*`,
-      `!${rootPath}/tests/**/*.*`,
-      `!${rootPath}/vendor/**/*.*`,
-      `!${rootPath}/dst/**/*.*`,
-      `!${rootPath}/core/Extras/kirki/**/*.*`,
-    ],
-    dest: `${rootPath}/languages/${names.slug}.pot`,
-  },
+    languages: {
+        src: [
+            `${rootPath}/**/*.php`,
+            `!${rootPath}/optional/**/*.*`,
+            `!${rootPath}/tests/**/*.*`,
+            `!${rootPath}/vendor/**/*.*`,
+            `!${rootPath}/dst/**/*.*`,
+            `!${rootPath}/core/Extras/kirki/**/*.*`,
+        ],
+        dest: `${rootPath}/languages/${names.slug}.pot`,
+    },
 };
 
 export { paths };
