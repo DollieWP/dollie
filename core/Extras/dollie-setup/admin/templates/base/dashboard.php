a82ee2d584772134cf908cef8270f07b29ac3ede
Bowe Frankema <bowe@cfcommunity.net>
2023-05-09 11:54:34 +0200

    Move settings to main screen
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Extras/dollie-setup/admin/templates/base/dashboard.php
+++ core/Extras/dollie-setup/admin/templates/base/dashboard.php
@@ -1,20 +1,33 @@
+<?php acf_form_head(); ?>
 <div class="dol-min-h-full dol-mt-8 dol-pt-[100px]">
-	<main class="dol-pb-8 dol--mt-24">
-		<div class="dol-max-w-3xl dol-px-4 dol-mx-auto sm:dol-px-6 lg:dol-max-w-7xl lg:dol-px-8">
-			<h1 class="dol-sr-only">Profile</h1>
-			<div class="dol-grid dol-items-start dol-grid-cols-1 dol-gap-4 lg:dol-grid-cols-3 lg:dol-gap-8">
-				<div class="dol-grid dol-grid-cols-1 dol-gap-4 lg:dol-col-span-2">
-					<?php dollie_setup_get_template_part( 'welcome-header' ); ?>
-					<?php dollie_setup_get_template_part( 'setup-complete' ); ?>
+    <main class="dol-pb-8 dol--mt-24">
+        <div class="dol-px-4 dol-mx-auto">
+            <h1 class="dol-sr-only">Profile</h1>
+            <div class="dol-grid dol-items-start dol-grid-cols-1 dol-gap-4 lg:dol-grid-cols-3 lg:dol-gap-8">
+                <div class="dol-grid dol-grid-cols-1 dol-gap-4 lg:dol-col-span-2">
+                    <?php dollie_setup_get_template_part('welcome-header'); ?>
+					<div class="dol-bg-white dol-p-5">
+                    <?php
+                    $args = [
+                        'post_id' => 'options',
+                        'field_groups' => ['group_5ada1549129fb'], // this is the ID of the field group
+                    ];
+                    acf_form($args);
+                    ?>
+					</div>
 
-					<?php dollie_setup_get_template_part( 'recent-sites' ); ?>
-				</div>
 
-				<div class="dol-grid dol-grid-cols-1 dol-gap-4">
-					<?php dollie_setup_get_template_part( 'recent-customers' ); ?>
-					<?php dollie_setup_get_template_part( 'dollie-plugins' ); ?>
-				</div>
-			</div>
-		</div>
-	</main>
+                    <?php dollie_setup_get_template_part('recent-sites'); ?>
+                </div>
+
+
+
+                <div class="dol-grid dol-grid-cols-1 dol-gap-4">
+                    <?php dollie_setup_get_template_part('setup-complete'); ?>
+                    <?php dollie_setup_get_template_part('recent-customers'); ?>
+                    <?php dollie_setup_get_template_part('dollie-plugins'); ?>
+                </div>
+            </div>
+        </div>
+    </main>
 </div>
