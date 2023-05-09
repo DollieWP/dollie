0803eab8725f063592d35a0da365737afbfc46f5
Bowe Frankema <bowe@cfcommunity.net>
2023-05-09 13:51:53 +0200

    Small design + wording changes
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- templates/admin/templates.php
+++ templates/admin/templates.php
@@ -6,19 +6,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 ?>
 <div class="dol-container dol-mr-6 dol-mt-6 dol-ml-3">
 
-    <div class="dol-mx-auto dol-bg-white dol-rounded dol-shadow-lg dol-overflow-hidden dol-mb-6">
-        <div class="dol-flex dol-flex-wrap dol-justify-center md:dol-justify-between dol-items-center dol-p-8">
+    <div class="dol-mx-auto dol-overflow-hidden dol-mb-6">
+        <div class="dol-flex dol-flex-wrap dol-justify-center md:dol-justify-between dol-items-center">
             <div class="dol-text-center md:dol-text-right">
-                <h1 class="dol-block dol-leading-normal dol-my-0"><span class="dol-font-light">Dollie</span> Templates</h1>
+                <h1 class="dol-block dol-leading-normal dol-my-0"><span class="dol-font-light">Your Hub</span> Templates</h1>
             </div>
-
-            <a href="#" target="_blank" rel="noopener"
-               class="dol-text-base dol-flex dol-items-center dol-content-center dol-no-underline focus:dol-shadow-none">
-                <span class="dol-inline-block">
-                    <img src="<?php echo DOLLIE_ASSETS_URL; ?>/img/dollie-logo.png" alt="Dollie" class="dol-w-[100px]">
-                </span>
-            </a>
-
 
         </div>
     </div>
@@ -33,7 +25,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<?php esc_html_e( 'Here you can import ready to use page templates for your preferred builder.', 'dollie' ); ?>
     </div>
     <div class="dol-text-md dol-text-gray-700 dol-mt-2 ">
-    Importing a template will add a number of pages to your site and assign them to dollie specific areas.
+    Importing our pre-made template will automatically add the Hub Pages to your site. Once imported you can customise them to your needs further.
     </div>
     <div class="dol-mt-5">
         <div class="dol-grid dol-grid-cols-1 md:dol-grid-cols-2 lg:dol-grid-cols-3 dol-gap-4">
