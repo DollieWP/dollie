e4590613d9cd7a81a41b006d7399d4097e51ca42
Bowe Frankema <bowe@cfcommunity.net>
2023-06-23 16:46:40 +0200

    removed unneeded paramters
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Modules/Subscription/Plugin/SubscriptionInterface.php
+++ core/Modules/Subscription/Plugin/SubscriptionInterface.php
@@ -15,6 +15,6 @@ interface SubscriptionInterface {
 
 	public function redirect_to_blueprint( $id );
 	public function get_checkout_link( $args );
-	public function get_customer_subscriptions( $status = null, $customer_id = null );
+	public function get_customer_subscriptions( $customer_id = null );
 	public function has_bought_product( $user_id = 0 );
 }
