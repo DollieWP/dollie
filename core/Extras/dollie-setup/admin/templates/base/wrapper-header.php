bdc068ed946dd8570afa4f41db90151d833dff0a
Bowe Frankema <bowe@cfcommunity.net>
2023-06-26 16:19:03 +0200

    Styling
    Signed-off-by: Bowe Frankema <bowe@cfcommunity.net>

--- core/Extras/dollie-setup/admin/templates/base/wrapper-header.php
+++ core/Extras/dollie-setup/admin/templates/base/wrapper-header.php
@@ -1,42 +1,17 @@
-<div class="dol-dashboard-wrap">
-	<div class="dol-relative bg-sky-700 dol-pb-32 dol-overflow-hidden">
-		<nav class="dol-bg-transparent dol-relative dol-z-10 dol-border-b dol-border-teal-500 dol-border-opacity-25 lg:dol-bg-transparent lg:dol-border-none dol-admin-navigation">
-			<div class="dol-max-w-7xl dol-mx-auto dol-px-2 sm:dol-px-4 lg:dol-px-8">
-				<div class="dol-relative dol-h-16 dol-flex dol-items-center dol-justify-between lg:dol-border-b lg:border-sky-800">
-					<div class="dol-px-2 dol-flex dol-items-center lg:dol-px-0">
-						<div class="dol-flex-shrink-0">
-							<img class="dol-block dol-h-8 dol-w-auto" src="<?php echo DOLLIE_ASSETS_URL; ?>img/active.png" alt="Workflow">
-						</div>
-						<div class="dol-hidden lg:dol-block lg:dol-ml-6 lg:dol-space-x-4">
-							<div class="dol-flex">
-								<!-- Current: "dol-bg-black dol-bg-opacity-25", Default: "hover:bg-sky-800" -->
-								<a href="#" class="dol-bg-black dol-bg-opacity-25 dol-rounded-md dol-py-2 dol-px-3 dol-text-sm dol-font-medium dol-text-white">Dashboard</a>
-
-								<a href="#" class="hover:bg-sky-800 dol-rounded-md dol-py-2 dol-px-3 dol-text-sm dol-font-medium dol-text-white">Jobs</a>
-
-								<a href="#" class="hover:bg-sky-800 dol-rounded-md dol-py-2 dol-px-3 dol-text-sm dol-font-medium dol-text-white">Applicants</a>
-
-								<a href="#" class="hover:bg-sky-800 dol-rounded-md dol-py-2 dol-px-3 dol-text-sm dol-font-medium dol-text-white">Company</a>
-							</div>
-						</div>
-					</div>
-
-				</div>
-			</div>
-		</nav>
+<div class="dol-dashboard-wrap p-0" style="
+	position: relative;
+	left: -20px;
+	right: -20px;
+	margin-right: -20px;
+	/* width: 101%; */
+">
+	<div class="dol-relative bg-sky-700 dol-pb-32">
 		<div aria-hidden="true" class="dol-inset-y-0 dol-absolute dol-inset-x-0 dol-left-1/2 dol-transform dol--translate-x-1/2 dol-w-full dol-overflow-hidden lg:dol-inset-y-0">
 			<div class="dol-absolute dol-inset-0 dol-flex">
-				<div class="dol-h-full dol-w-1/2" style="background-color: #0a527b"></div>
-				<div class="dol-h-full dol-w-1/2" style="background-color: #065d8c"></div>
+				<div class="dol-h-full dol-w-1/2" style="background-color: #31A7BE"></div>
+				<div class="dol-h-full dol-w-1/2" style="background-color: #31A7BE"></div>
 			</div>
-			<div class="dol-relative dol-flex dol-justify-center">
-				<svg class="dol-flex-shrink-0" width="1750" height="308" viewBox="0 0 1750 308" xmlns="http://www.w3.org/2000/svg">
-					<path d="M284.161 308H1465.84L875.001 182.413 284.161 308z" fill="#32A6BD" />
-					<path d="M1465.84 308L16.816 0H1750v308h-284.16z" fill="#EFEFEF" />
-					<path d="M1733.19 0L284.161 308H0V0h1733.19z" fill="#32A6BD" />
-					<path d="M875.001 182.413L1733.19 0H16.816l858.185 182.413z" fill="#32A6BD" />
-				</svg>
-			</div>
+
 		</div>
 		<header class="dol-relative dol-py-10">
 			<div class="dol-flex dol-items-center dol-space-x-4 dol-max-w-7xl dol-mx-auto dol-px-4 sm:dol-px-6 lg:dol-px-8">
